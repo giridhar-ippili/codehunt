4a8df26de1821d70df5baee6f24251a78bb90705
giridhar-ippili <giridhar.ippili@hotmail.com>
2020-04-09 14:55:23 -0400

    Adding props

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-function Tile() {
+function Tile(props) {
   return (
     <div className='note'>
-        <h3>Title here</h3>
-        <p>Hello there</p>
+        <h3>{props.name}</h3>
+        <img src={props.img} alt='contact-img'></img>
+        <p>{props.phone}</p>
+        <p>{props.email}</p>
     </div>  
   );
 }
