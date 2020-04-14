91690bf382a6ed2902224627d55f0ca559de7d5e
giridhar-ippili <giridhar.ippili@hotmail.com>
2020-04-14 12:51:44 -0400

    add state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Popup from 'reactjs-popup';
 import Form from './Form'
 
-
 function Header() {
+  setInterval(getTime,1000)
+  const now = new Date().toLocaleTimeString()
+  const [time, setTime] = useState(now)
+  function getTime(){
+    const currentTime = new Date().toLocaleTimeString()
+    setTime(currentTime)
+  }
   return (
     <header>
+    <div className="header-content">
       <h1>Buddies</h1> 
+      <h2>{time}</h2>    
       <Popup 
         modal 
         trigger={<button className="add-button">Add +</button>} 
@@ -16,6 +24,7 @@ function Header() {
         <Form />
       </div>
       </Popup>
+    </div>
     </header>    
   );
 }
